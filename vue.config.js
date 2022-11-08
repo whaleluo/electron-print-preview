6f5bece9b431ef1452e79ff32a2744f14d4ea9df
whaleluo <whaleluo@163.com>
2022-11-08 15:53:29 +0800

    add preload

--- vue.config.js
+++ vue.config.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   pluginOptions: {
     electronBuilder: {
+      preload: {preload: 'src/main/preload.ts'},
       // option: default // description
       disableMainProcessTypescript: false, // Manually disable typescript plugin for main process. Enable if you want to use regular js for the main process (src/background.js by default).
       mainProcessTypeChecking: false, // Manually enable type checking during webpack bundling for background file.
